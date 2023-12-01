15afe8d2740b619fed2fa524377f271428a685bc
Nur Wahid Muhammad <nwahidm@gmail.com>
2023-12-01 14:50:16 +0700

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ let exec_mode = "fork";
 module.exports = {
   apps: [
     {
-      name: "Test",
-      cwd: "/home/clouduser/backend/Test",
+      name: "Backend Service",
+      cwd: "/home/AudioCentre-Backend",
       script: "npm start",
       exp_backoff_restart_delay: 100,
       instances,
@@ -15,7 +15,7 @@ module.exports = {
       max_memory_restart: "1G",
       autorestart: true,
       env: {
-        PORT: 3070,
+        PORT: 3000,
         MONGO_URL:
           "mongodb+srv://AUDIOCENTRE:ENoNDR9rhgkvCosI@dbprod.uykdgyl.mongodb.net/?retryWrites=true&w=majority",
         EMAIL: "nwahidm@gmail.com",
